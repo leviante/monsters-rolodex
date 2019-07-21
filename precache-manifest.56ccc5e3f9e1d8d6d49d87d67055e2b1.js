463f751ba792b1f901fb62d20781a6edf4ca4952
Berk Açıkgöz <acikgozberk96@gmail.com>
2019-07-21 13:34:40 +0300

    Updates

---
+++ precache-manifest.56ccc5e3f9e1d8d6d49d87d67055e2b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50c270c85acaeff4e4c50c423f4c364",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "151f0478cf7b84fe05ef",
+    "url": "/monsters-rolodex/static/css/main.17eb8376.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "151f0478cf7b84fe05ef",
+    "url": "/monsters-rolodex/static/js/main.0a65f812.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
